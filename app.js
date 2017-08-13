3a567362188165c23c50a9e3c736bd997af4ea81
Melinda Fernandes <melindafernandes1993@gmail.com>
2017-08-13 13:31:29 +0100

    first riddle with only correct answer

--- app.js
+++ app.js
@@ -1,25 +1,54 @@
 $(function(event){
 	// temp hiding
-	$('.home-wrapper').hide();
-	$('.game-wrapper').show();
+	// $('.home-wrapper').hide();
+	// $('.game-wrapper').show();
 
+	//count riddles, for log active objects
+	var $riddleCount = 1;
 
 	// hide divs
-	//$('.game-wrapper').hide();
+	$('.game-wrapper').hide();
 	// get username
 	// setup event listener for start button
-	$("#start").click(function(){
-        var $username = $("input:text").val();
-        console.log($username)
-        console.log('start clicked')
-        // function to hide section on start click
-        $('.home-wrapper').hide();
-        console.log('home hidden');
-        // function to show gameplay div
-        $('.game-wrapper').show();
-    });
+	function start($riddleCount){
+		$('#start').click(function(){
+	        var $username = $('input:text').val();
+	        console.log($username)
+	        console.log('start clicked')
+	        // function to hide section on start click
+	        $('.home-wrapper').hide();
+	        console.log('home hidden');
+	        // function to show gameplay div
+	        $('.game-wrapper').show();
+	    });	
+	}	
 	
+	if ($riddleCount === 1){}
+		riddle1EventListeners($riddleCount);
 
+	//
 
+	function riddle1EventListeners(){
+		// setup correct room object event listeners
+		$('#umbrella').click(function(){
+			// update banner message
+			$('.display-message').html('CORRECT!');
+			// call function to generate new riddle
+			setTimeout(function(){
+				generateRiddle($riddleCount);
+			}, 2000);
+		})
 
+		// setup incorrect room object even listeners
+	}
+		
+
+	function generateRiddle($riddleCount){
+			$riddleCount ++;
+			if ($riddleCount === 2) {
+				$('.display-message').html('new riddle');
+			}
+		}
+
+	start();
 })
